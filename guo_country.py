530ac11eb714144d1ee36ee5aec17d9d1687b319
Fasther <30392053+Fasther@users.noreply.github.com>
2019-03-10 12:45:46 +0100

    Gettin results with GUO and iso country counts

--- guo_country.py
+++ guo_country.py
@@ -1,8 +1,17 @@
 import csv
 
-with open("data3.csv", "r") as file:
+csv.register_dialect("excel", delimiter=";")
+with open("training.csv", "r") as file:
     contents = csv.DictReader(file, dialect="excel")
-
+    rowno = 0
     results = {}
     for row in contents:
         data = dict(row)
+        results.setdefault(data["GUO - BvD ID number"], dict())
+        results[data["GUO - BvD ID number"]].setdefault(data["Country ISO code"], 0)
+        results[data["GUO - BvD ID number"]][data["Country ISO code"]] += 1
+        rowno += 1
+    file.close()
+
+for guo, countries in results.items():
+    print(guo, countries)
