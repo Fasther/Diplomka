6ae06701eb7db8d8ccd24b114e0ba44a68dc424c
Fasther <30392053+Fasther@users.noreply.github.com>
2019-03-10 20:06:00 +0100

    save results to CSV

--- guo_country.py
+++ guo_country.py
@@ -1,6 +1,7 @@
 import csv
 
 csv.register_dialect("excel", delimiter=";")
+print("Getting and sorting data...")
 with open("training.csv", "r") as file:
     contents = csv.DictReader(file, dialect="excel")
     rowno = 0
@@ -11,10 +12,22 @@ with open("training.csv", "r") as file:
         results[data["GUO - BvD ID number"]].setdefault(data["Country ISO code"], 0)
         results[data["GUO - BvD ID number"]][data["Country ISO code"]] += 1
         rowno += 1
-    file.close()
+file.close()
+print("Getting counts...")
+final_data = []
 
 for guo, countries in results.items():
     k = list(countries.keys())
     v = list(countries.values())
-    country = k[v.index(max(v))]
-    print(guo, country)
+    country_iso = k[v.index(max(v))]
+    temp_data = dict({"GUO": guo, "Country ISO code": country_iso})
+    final_data.append(temp_data)
+    #
+print("Writing data...")
+
+with open("result_guo.csv", "w", newline='') as result_file:
+    writer = csv.DictWriter(result_file, fieldnames=["GUO", "Country ISO code"], dialect="excel")
+    writer.writeheader()
+    writer.writerows(final_data)
+
+print("Work done!")
